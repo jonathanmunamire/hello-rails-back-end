a61b6f407fda5d1cdbf4b4155027dd8a6d5a45c2
jonathanmunamire <jonathanmunamire@gmail.com>
2023-04-21 19:33:47 +0200

    Add CORS

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
     def index
-      render json: { greeting: Message.order("RANDOM()").first.greeting }
+      render json: { greeting: Message.order("RANDOM()").first }
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :messages, only: [:index]
 
   # Defines the root path route ("/")
-  root "messages#index"
+  # root "messages#index"
 end
